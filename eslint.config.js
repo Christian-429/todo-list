a1377f1063a705009f583b05df667298db00120c
Christian Jimenez <jimenezchristian429@gmail.com>
2025-11-17 10:49:57 -0800

    Created Todo app title and structure

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 import js from '@eslint/js'
 import globals from 'globals'
+import react from 'eslint-plugin-react';
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
@@ -13,6 +14,16 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    settings: { 
+      react: {
+         version: 'detect' 
+        } 
+      },
+      plugins: {
+        react,
+        reactHooks,
+      },
+
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,7 +34,9 @@ export default defineConfig([
       },
     },
     rules: {
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      //'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': 'warn', //this changes the error to a warning
+      'react/prop-types': 'off',
     },
   },
 ])
